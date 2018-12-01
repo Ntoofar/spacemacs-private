8ae427c3b75668d0cfb1749aea9c15e9b0f03539
Ntoofar <ntoofar@gmail.com>
2018-12-01 08:00:10 +0800

    add go layer

--- init.el
+++ init.el
@@ -54,6 +54,10 @@ values."
      scala
      yaml
      asciidoc
+     (go :variables
+         go-tab-width 4
+         gofmt-command "goimports"
+         go-use-gometalinter t)
      )
    ;; List of additional packages that will be installed without being
    ;; wrapped in a layer. If you need some configuration for these
